a6398e761b143ccaf19a8a61c9011659569da300
juanchete <juanlopezlmg@gmail.com>
2024-02-22 13:25:06 -0400

    feat: Home Screen and CircleGroupView

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Juan Lopez on 22/2/24.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGroupView()
+}
