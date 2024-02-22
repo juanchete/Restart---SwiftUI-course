b44264baa04008d05a1e2dc192f9d22b79847eda
juanchete <juanlopezlmg@gmail.com>
2024-02-22 12:13:57 -0400

    feat: Build the Onboarding Screen

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct HomeView: View {
+    // MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20){
+            Text("Home")
+                .font(.largeTitle)
+            Button(action: {
+                //Some action
+                isOnboardingViewActive = true
+            }) {
+                Text("Restart")
+            }
+        }
     }
 }
 
